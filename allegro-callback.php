72aa431855d7b414407acad2031cc287a58174fd
Igor Pabiańczyk <49597012+ipabianczyk@users.noreply.github.com>
2026-06-24 20:03:11 +0200

    Delete allegro-callback.php

--- allegro-callback.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Ten plik po prostu wyświetli kod, który wyśle do Ciebie Allegro
-if (isset($_GET['code'])) {
-    echo "Twój kod autoryzacyjny to: " . htmlspecialchars($_GET['code']);
-} else {
-    echo "Brak kodu w adresie URL.";
-}
-?>
